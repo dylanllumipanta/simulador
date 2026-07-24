c6344cf06e5e67f43372025a07c1fc7dbaea71ac
dylanllumipanta <dylanlumipanta2011@gmail.com>
2026-07-23 19:38:46 -0500

    cambios para examen

--- funciones.js
+++ funciones.js
@@ -9,7 +9,7 @@ function calcularDisponible(ingresos, egresos) {
 }
 
 function calcularCapacidadPago(montoDisponible) {
-    return montoDisponible * 0.5;
+    return montoDisponible * 0.30;
 }
 
 function calcularInteresSimple(monto, tasa, plazoAnios) {
